db50b55a0a251365e985b632038c3f6a330b3057
devanshu09 <dshukla1234fz@gmail.com>
2025-08-01 00:25:00 +0530

    5 question on os updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className=" py-4 bg-gray-100 text-center text-xs text-gray-500 m-0">
-  Made with love by Devanshu Shukla · © {new Date().getFullYear()} MCQ Portal
+  Made with ❤️ by Devanshu Shukla · © {new Date().getFullYear()} MCQ Portal
 </footer>
 
   );
